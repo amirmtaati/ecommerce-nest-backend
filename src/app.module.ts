2cd74044f8a9e34a9b949eb483264b98c290cdd2
amirMohammadTaati <mamad198585@gmail.com>
2022-03-26 13:54:15 +0430

    Graphql and Apollo added to app

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
 import { ProductsModule } from './products/products.module';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 
 @Module({
-    imports: [ProductsModule],
-    controllers: [],
-    providers: [],
+    imports: [
+        ProductsModule,
+        GraphQLModule.forRoot<ApolloDriverConfig>({
+            driver: ApolloDriver,
+            typePaths: ['./**/*.graphql'],
+        }),
+    ],
 })
 export class AppModule {}

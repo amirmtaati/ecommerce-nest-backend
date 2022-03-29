5a1ad19b1b250c4e54d4b37e558dd21b9f6a1c00
amirMohammadTaati <mamad198585@gmail.com>
2022-03-29 09:46:57 +0430

    graphql types updated

--- src/graphql.ts
+++ src/graphql.ts
@@ -7,6 +7,22 @@
 
 /* tslint:disable */
 /* eslint-disable */
+export class User {
+    username?: Nullable<string>;
+    email?: Nullable<string>;
+    password?: Nullable<string>;
+}
+
+export abstract class IMutation {
+    abstract signup(username: string, email: string, password: string, passConfirm: string): Nullable<User> | Promise<Nullable<User>>;
+
+    abstract createProduct(title: string, desc: string, price: number): Nullable<Product> | Promise<Nullable<Product>>;
+
+    abstract deleteProduct(id: string): Nullable<Product> | Promise<Nullable<Product>>;
+
+    abstract updateProduct(id: string, title?: Nullable<string>, desc?: Nullable<string>, price?: Nullable<number>): Nullable<Product> | Promise<Nullable<Product>>;
+}
+
 export class Product {
     title: string;
     desc?: Nullable<string>;
@@ -17,12 +33,6 @@ export abstract class IQuery {
     abstract products(): Nullable<Nullable<Product>[]> | Promise<Nullable<Nullable<Product>[]>>;
 
     abstract getProduct(id: string): Nullable<Product> | Promise<Nullable<Product>>;
-}
-
-export abstract class IMutation {
-    abstract createProduct(title: string, desc: string, price: number): Nullable<Product> | Promise<Nullable<Product>>;
-
-    abstract deleteProduct(id: string): Nullable<Product> | Promise<Nullable<Product>>;
 }
 
 type Nullable<T> = T | null;

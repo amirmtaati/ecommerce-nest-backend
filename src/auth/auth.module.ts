64a2cec7e794f2a36ceae446606d88c6f7ff954b
amirMohammadTaati <mamad198585@gmail.com>
2022-04-06 17:07:46 +0430

    auth module update

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,22 @@
-import { Module } from "@nestjs/common";
-import { AuthService } from "./auth.service";
-import { AuthResolver } from "./auth.resolver";
+import { AuthResolver } from './auth.resolver';
+import { AuthService } from './auth.service';
+import { JwtModule } from '@nestjs/jwt';
+import { JwtStrategy } from './strategies/jwt.strategy';
+import { LocalStrategy } from './strategies/local.strategy';
+import { Module } from '@nestjs/common';
+import { PassportModule } from '@nestjs/passport';
+import { UsersModule } from 'src/users/users.module';
+import { RolesGuard } from './guards/roles.guard';
 
 @Module({
-    controllers: [],
-    providers: [AuthService , AuthResolver]
+  imports: [
+    UsersModule,
+    PassportModule,
+    JwtModule.register({
+      secret: process.env.JWT_SECRET,
+    }),
+  ],
+  providers: [AuthService, AuthResolver, JwtStrategy, LocalStrategy , RolesGuard],
+  exports: [AuthService],
 })
 export class AuthModule {}

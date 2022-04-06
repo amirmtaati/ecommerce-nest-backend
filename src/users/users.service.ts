8958f1320fcd17bc2cf962e57f53ee06b18641b5
amirMohammadTaati <mamad198585@gmail.com>
2022-04-06 17:08:05 +0430

    users service update

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -5,4 +5,36 @@ import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class UsersService {
+    async findOne(email: string) {
+        try {
+            const user = await prisma.user.findUnique({
+                where: {
+                    email,
+                },
+            });
+
+            return user;
+        } catch (err) {
+            throw new HttpException("Can't find user", 400);
+        }
+    }
+
+    async createOne(username: string, email: string, password: string) {
+        try {
+            const salt = await bcrypt.genSalt();
+            const hash = await bcrypt.hash(password, salt);
+
+            const user = await prisma.user.create({
+                data: {
+                    username,
+                    email,
+                    password: hash,
+                },
+            });
+
+            return user;
+        } catch (err) {
+            throw new HttpException("Can't create user", 400);
+        }
+    }
 }

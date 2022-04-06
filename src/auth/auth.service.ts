41c4886c3a7cf232eb900d188a8da3a93fded648
amirMohammadTaati <mamad198585@gmail.com>
2022-04-06 17:06:48 +0430

    auth service update

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,26 +1,58 @@
 import { Injectable } from '@nestjs/common';
-import { prisma } from '../../prisma';
 import { HttpException } from '@nestjs/common';
+import { UsersService } from 'src/users/users.service';
+import { JwtService } from '@nestjs/jwt';
 import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class AuthService {
+    constructor(
+        private readonly jwtService: JwtService,
+        private readonly userService: UsersService,
+    ) {}
+
     async signup(username: string, email: string, password: string) {
         try {
-            const salt = await bcrypt.genSalt();
-            const hash = await bcrypt.hash(password, salt);
-
-            const newUser = await prisma.user.create({
-                data: {
-                    username,
-                    email,
-                    password: hash,
-                },
-            });
-
-            return newUser;
+            return this.userService.createOne(username, email, password);
         } catch (err) {
-            throw new HttpException("Can't add user" , 400);
+            throw new HttpException("Can't add user", 400);
+        }
+    }
+
+    async validateUser(email: string, password: string) {
+        try {
+            const user = await this.userService.findOne(email);
+
+            const compare = await bcrypt.compare(password, user.password);
+
+            return compare;
+        } catch (err) {
+            throw new HttpException("Can't validate user", 400);
+        }
+    }
+
+    async login(email: string, password: string) {
+        try {
+            const validate = await this.validateUser(email, password);
+
+            if (validate) {
+                const user = await this.userService.findOne(email);
+                
+                const payload = {
+                    user
+                };
+
+                const { password , ...result} = user;
+
+                return {
+                    token: this.jwtService.sign(payload),
+                    user : result,
+                };
+            } else {
+                throw new HttpException('Cant login', 400);
+            }
+        } catch (err) {
+            throw new HttpException(err, 400);
         }
     }
 }

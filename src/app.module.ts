79b56422d8457c103db91783da1645ad53776ac1
amirMohammadTaati <mamad198585@gmail.com>
2022-04-06 17:08:55 +0430

    app module updated

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,18 +1,22 @@
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { AuthModule } from './auth/auth.module';
+import { ConfigModule } from '@nestjs/config';
+import { GraphQLModule } from '@nestjs/graphql';
 import { Module } from '@nestjs/common';
 import { ProductsModule } from './products/products.module';
 import { UsersModule } from './users/users.module';
-import { AuthModule } from './auth/auth.module';
-import { GraphQLModule } from '@nestjs/graphql';
-import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 
 @Module({
     imports: [
+        AuthModule,
         ProductsModule,
         UsersModule,
-        AuthModule,
         GraphQLModule.forRoot<ApolloDriverConfig>({
             driver: ApolloDriver,
             typePaths: ['./**/*.graphql'],
+        }),
+        ConfigModule.forRoot({
+            isGlobal: true,
         }),
     ],
 })

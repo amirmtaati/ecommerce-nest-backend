32a55243365aa55d0a243fcebb3e86a3769c9858
amirMohammadTaati <mamad198585@gmail.com>
2022-04-15 16:17:13 +0430

    add to cart added into resolver

--- src/users/users.resolver.ts
+++ src/users/users.resolver.ts
@@ -1,6 +1,18 @@
-import { Resolver, Mutation, Query, Args } from '@nestjs/graphql';
+import { Resolver, Mutation, Args } from '@nestjs/graphql';
+import { ExecutionContext } from '@nestjs/common';
+import { GqlExecutionContext , Context } from '@nestjs/graphql';
 import { UsersService } from './users.service';
+import { Product } from 'src/graphql';
+import {UseGuards} from '@nestjs/common';
+import {JwtAuthGuard} from 'src/auth/guards/jwt.guard';
 
-@Resolver('User')
+@Resolver()
 export class UsersResolver {
+    constructor(private readonly userService: UsersService) {}
+
+    @Mutation(() => Product)
+    @UseGuards(JwtAuthGuard)
+    async addToCart(@Args('id') id: string , @Context() ctx:any) {
+        return this.userService.addToCart(ctx.req.user.email, id);
+    }
 }

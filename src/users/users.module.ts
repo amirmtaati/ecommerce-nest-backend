47e7c18d8c49b9037814b6e9f452a1f48fb20bc9
amirMohammadTaati <mamad198585@gmail.com>
2022-04-15 16:17:22 +0430

    user module updated

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -3,7 +3,6 @@ import { UsersService } from "./users.service";
 import { UsersResolver } from "./users.resolver";
 
 @Module({
-    controllers: [],
     exports: [UsersService],
     providers: [UsersService , UsersResolver]
 })
